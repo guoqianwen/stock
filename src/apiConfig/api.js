e9809096d2c6e7ba4a62c58b78becca54a42cfe4
xio <708894859@qq.com>
2018-09-03 22:10:19 +0800

    移动端屏幕适配

--- src/apiConfig/api.js
+++ src/apiConfig/api.js
@@ -1,4 +1,4 @@
-let localhostDev = false;
+let localhostDev = true;
 //如果是本地开发，就把localhostDev 设置成true，连接就是拼成‘http://localhost:8080/api/test’
 //生产环境，就把localhostDev 设置成false，连接就是拼成‘http://xxx.com/api/test’
 //let _host= localhostDev ? 'http://1000stock.com:8082' : 'https://www.1000stock.com';
